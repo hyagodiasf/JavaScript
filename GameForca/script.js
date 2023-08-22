0135205a1c794735657965cfd309712f42246854
hyagodiasf <hyagodiasf@gmail.com>
2023-08-22 19:53:42 -0300

    Mais JS
    Finalizando o código JS

--- GameForca/script.js
+++ GameForca/script.js
@@ -1,21 +1,35 @@
 const keyboard = document.querySelector('.keyboard');
 const key_word = document.querySelector('.key-word');
 
+let palavraAtual, errado = 0;
+const total = 6;
+
 const randomList = () => {
-    const { palavra, dica } = listaPalavra[Math.floor(Math.random()  * listaPalavra.length)];
+    const { palavra, dica } = listaPalavra[Math.floor(Math.random() * listaPalavra.length)];
     document.querySelector('.hint-text b').innerHTML = dica;
+    palavraAtual = palavra;
     key_word.innerHTML = palavra.split("").map(() => `<li class="letters"></li>`).join('');
 }
 
 const iniGame = (button, clickLetter) => {
-    console.log(button, clickLetter);
+    if (palavraAtual.includes(clickLetter)) {
+        [...palavraAtual].forEach((letra, index) => {
+            if (letra === clickLetter) {
+                key_word.querySelectorAll('li')[index].innerText = letra;
+                key_word.querySelectorAll('li')[index].classList.add('guessed')
+            }
+        })
+    } else {
+        errado++;
+    }
+    document.querySelector('.guesses-text b').innerText = `${errado} / ${total}`;
 }
 
 for (let i = 97; i <= 122; i++) {
     const button = document.createElement('button');
     button.innerText = String.fromCharCode(i);
     keyboard.appendChild(button);
-    button.addEventListener('click', e => iniGame(e.target, String.fromCharCode(i)));   
+    button.addEventListener('click', e => iniGame(e.target, String.fromCharCode(i)));
 }
 
 randomList()
